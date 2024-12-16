84993708f4a5326f9d852f4ef19620320c8b45cd
MehranGhasabeh <mehran.ghasabeh@gmail.com>
2024-12-16 15:18:30 +0100

    FM1 model

---
+++ run.sh
@@ -0,0 +1,10 @@
+##!/bin/bash
+
+rm -f *_ts_* *.pvd *~ \#*
+# mkdir -p ./_out_PardisoLU_12_cores
+# mkdir -p ./_out_PETSc_12_cores
+mkdir -p ./_out_PESTc_single_core
+
+# OMP_NUM_THREADS=12 OGS_ASM_THREADS=12 ~/build/release/bin/ogs -o _out_PardisoLU_12_cores FM1.prj > _out_PardisoLU_12_cores/out_PardisoLU_12_cores.txt
+# mpiexec -n 12 ~/build/release-petsc/bin/ogs FM1_1.0e-16.prj -o _out_PETSc_12_cores  > _out_PETSc_12_cores/out_PETSc_12_cores.txt
+~/build/release-petsc/bin/ogs FM1.prj -o _out_PESTc_single_core  > _out_PESTc_single_core/out_PESTc_single_core.txt
